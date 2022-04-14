300089c4e0d2a3cdc195aec9ea29065be4941ec5
fsdio77 <103621006+fsdio77@users.noreply.github.com>
2022-04-14 11:54:19 +0600

    Add files via upload

---
+++ laba/2.3.py
@@ -0,0 +1,5 @@
+импортировать  математику
+x = int ( input ( "Ввести значение x:" ))
+t = int ( input ( "Ввести значение t:" ))
+z = (( 9 * math . pi * t + 10 * math . cos ( x )) / ( math . sqrt ( t ) - math . fabs ( math . sin ( t )))) * math . pow ( математика e , x ) _
+печать ( "z={0:.2f}" . формат ( z ))
